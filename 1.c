d3e06232ab2c19036f2b0217a51eb3c90b787db0
pikapikale <2236723841@qq.com>
2022-11-23 22:55:39 +0800

    第一次自己尝试提交代码

---
+++ 1.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int input = 0;//输入的值
+	printf("加入比特:>\n");
+	printf("那要好好学习吗(1/0)?>:");
+	scanf_s("%d", &input);
+
+	if (input == 1)
+	{
+		printf("好offer\n");
+	}
+	else
+	{ 
+		printf("卖红薯\n");
+	}
+	return 0;
+}
